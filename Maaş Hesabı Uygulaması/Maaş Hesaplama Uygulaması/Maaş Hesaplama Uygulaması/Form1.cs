8034501ba975be3194b302b8c0c33db81d43d76c
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2022-12-17 01:48:27 +0300

    Update Form1.cs
    Update

--- Maaş Hesabı Uygulaması/Maaş Hesaplama Uygulaması/Maaş Hesaplama Uygulaması/Form1.cs
+++ Maaş Hesabı Uygulaması/Maaş Hesaplama Uygulaması/Maaş Hesaplama Uygulaması/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using MaasHesaplama_Dll;
 
@@ -9,19 +9,16 @@ namespace Maaş_Hesaplama_Uygulaması
         MaasH maasHesaplama = new MaasH();
         int n;
 
-
         public Form1()
         {
             InitializeComponent();
         }
-
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (checkBox1.Checked)
                 textBox2.Enabled = true;
             else textBox2.Enabled = false;
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -35,13 +32,16 @@ namespace Maaş_Hesaplama_Uygulaması
                 checkBox3.Checked = false;
             maasHesaplama.DtEkle(double.Parse(textBox1.Text), netMaas, checkBox1.Checked, checkBox2.Checked, int.Parse(textBox2.Text));
             MessageBox.Show("Kayıt yapıldı!");
-
         }   
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            n = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Kaç personel gireceksiniz?", "Personel sayısı","0"));
-            checkBox3.Checked = true;
+
+            if (int.TryParse(Microsoft.VisualBasic.Interaction.InputBox("Kaç personel gireceksiniz?", "Personel sayısı"), out int n))
+                checkBox3.Checked = true; 
+            else checkBox3.Checked = false;
+
+            button1.Enabled = checkBox3.Checked;
         }
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
