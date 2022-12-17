9d008d868dc4215e204ee0db4f93b13b0c0c424b
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2022-12-18 01:18:24 +0300

    Update Class1.cs

--- Maaş Hesabı Uygulaması/MaasHesaplama_Dll/MaasHesaplama_Dll/Class1.cs
+++ Maaş Hesabı Uygulaması/MaasHesaplama_Dll/MaasHesaplama_Dll/Class1.cs
@@ -1,33 +1,56 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 
-namespace MaasHesaplama_Dll
+namespace MaasHesaplamasi
 {
     public class MaasH
     {
-        SqlConnection con = new SqlConnection("Data Source=LENOVO\\SQLEXPRESS;Initial Catalog=maaslar;Integrated Security=True");
-        double VergiOrani = 20;
+        public static double Asgari = 6471;
+        double SgkTavan = Asgari * 7.5;
+
+        public static double VergiOrani = 20;
         double SigortaOrani = 15;
-        double net = 0;
 
-        public double NetMaas(double BrutMaas, bool EvliMi, bool Engellilik, int CocukSayisi)
+        double ek = 0;
+        byte asga = 1;
+
+        public double NetMaas1(bool Asgari, double BrutMaas, bool EvliMi, bool Engellilik, int CocukSayisi)
         {
-            double Vergi = (BrutMaas * VergiOrani) / 100;
-            double Sigorta = (BrutMaas * SigortaOrani) / 100;
+            if (Asgari) asga = 0;
+            double Vergi = (BrutMaas * VergiOrani * asga) / 100;
+            double Sigorta = BrutMaas >= SgkTavan ? (SgkTavan * SigortaOrani) / 100 : (BrutMaas * SigortaOrani) / 100;
 
-            if (EvliMi && CocukSayisi > 0)
-                net += CocukSayisi * 100;
             if (Engellilik)
-                net += 500;
-            double NetMaasimiz = BrutMaas - (Vergi + Sigorta) + net;
-            net = 0;
+                ek += 500;
+            double NetMaasimiz = BrutMaas - (Vergi + Sigorta) + ek;
+            if (EvliMi && CocukSayisi > 0)
+                ek += CocukSayisi * 100;
+            ek = 0;
+            return NetMaasimiz+ek;
+        }
+
+        public double NetMaas2(bool Asgari, double BrutMaas, bool Engellilik)
+        {
+            if (Asgari) asga = 0;
+            double Vergi = (BrutMaas * VergiOrani * asga) / 100;
+            double Sigorta = BrutMaas >= SgkTavan ? (SgkTavan * SigortaOrani) / 100 : (BrutMaas * SigortaOrani) / 100;
+
+            if (Engellilik) ek += 500;
+
+            double NetMaasimiz = BrutMaas - (Vergi + Sigorta) + ek;
+            ek = 0;
             return NetMaasimiz;
         }
+    }
+    public class DtEkleme
+    {
+        SqlConnection con = new SqlConnection("Data Source=LENOVO\\SQLEXPRESS;Initial Catalog=maaslar;Integrated Security=True");
+
         public void DtEkle(double BrutMaas, double Netmaass, bool EvliMi, bool Engellilik, int CocukSayisi)
         {
             con.Open();
             SqlCommand cmd = new SqlCommand("INSERT INTO PERSONEL(BURUTMAAS,NETMAAS,ENGELLILIK,EVLILIK,COCUKSAYISI) " +
-                "VALUES("+ BrutMaas + "," + Netmaass + ",'" + Engellilik + "','" + EvliMi + "'," + CocukSayisi + ")", con);
+                "VALUES(" + BrutMaas + "," + Netmaass + ",'" + Engellilik + "','" + EvliMi + "'," + CocukSayisi + ")", con);
             cmd.ExecuteNonQuery();
             con.Close();
         }
