f34e7e4c53f1e132378a99443cb833d8b469dd05
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2022-12-20 00:31:41 +0300

    Update Form1.cs

--- Maaş Hesabı Uygulaması/Güncel versiyon/MaasHesapUygulaması/MaasHesapUygulaması/Form1.cs
+++ Maaş Hesabı Uygulaması/Güncel versiyon/MaasHesapUygulaması/MaasHesapUygulaması/Form1.cs
@@ -17,42 +17,47 @@ namespace MaasHesapUygulaması
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if (int.TryParse(Microsoft.VisualBasic.Interaction.InputBox("Kaç personel gireceksiniz?", "Personel sayısı","0"), out int n) && n>0)
-                checkBox3.Checked = true;
-            else checkBox3.Checked = false;
+            if (int.TryParse(Microsoft.VisualBasic.Interaction.InputBox("Kaç personel gireceksiniz?", "Personel sayısı"), out n) && n > 0) checkBox3.Checked = true;
+            else
+            {
+                checkBox3.Checked = false;
+                n = 0;
+            }
             //Program oluşturulurken girilmek istenen kayıt sayısını önceden soran kod parçası
             button1.Enabled = checkBox3.Checked;
             textBox1.Enabled = false;
+            listView1.View = View.Details;
+            listView1.Columns.Add("Bürüt Maaş      |     Net Maaş",220);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (comboBox1.Enabled)
-                cocuks = int.Parse(comboBox1.Text);
+            if (comboBox1.Enabled) cocuks = int.Parse(comboBox1.Text);
             else cocuks = 0;
 
-
-            if (textBox1.Text != "" || radioButton2.Checked)
+            if (textBox1.Text != "" || radioButton2.Checked && n>=0)
             {
                 if (radioButton2.Checked) textBox1.Text = MaasH.Asgari.ToString();
 
-
-                double netMaas = maasHesaplama.NetMaas1(double.Parse(textBox1.Text), checkBox1.Checked, checkBox2.Checked, cocuks);
+                double netMaas = maasHesaplama.NetMaas(double.Parse(textBox1.Text), checkBox1.Checked, checkBox2.Checked, cocuks);
 
                 //net maaş hesaplamak için.
+                listView1.Items.Add(double.Parse(textBox1.Text) + "                   " + netMaas.ToString(),150); //Girilen verileri Listelemek için Veri tabanından veriler de çekilebilir.
 
-                listView1.Items.Add(netMaas.ToString()); //Girilen verileri Listelemek için
                 dt.DtEkle(textBox1.Text, netMaas.ToString().Replace(',','.'), checkBox1.Checked, checkBox2.Checked, cocuks);
                 //Dll üzerinden Database kaydedilmek istenen veriler...
+                MessageBox.Show(n.ToString());
                 n--;
-                MessageBox.Show("Kayıt yapıldı!");
+                MessageBox.Show(n.ToString());
 
+                MessageBox.Show("Kayıt yapıldı!");
+                if (n <= 0) checkBox3.Checked = false;
+                button1.Enabled = checkBox3.Checked;
             }
             else MessageBox.Show("Bürüt maaş kısmını doldurunuz!!!");
 
-            if (n == 0) //girmek istediğimiz kayıt sayısına ulaştıktan sonra devreye girecek koşul satırı.
-                checkBox3.Checked = false;
-            button1.Enabled = checkBox3.Checked;
+
+            //girmek istediğimiz kayıt sayısına ulaştıktan sonra devreye girecek koşul satırı.
         }
 
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
